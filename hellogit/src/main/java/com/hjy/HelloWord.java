2ae07a41de87e134ea3efdc0dbf10688f0dface5
glo_hjy <glo_hjy.@qq.com>
2020-09-18 09:36:58 +0800

    这是提交的第二个版本

--- hellogit/src/main/java/com/hjy/HelloWord.java
+++ hellogit/src/main/java/com/hjy/HelloWord.java
@@ -5,5 +5,6 @@ public class HelloWord {
     public static void main(String[] args) {
         System.out.print("第一个版本");
         System.out.println("第二个版本");
+        System.out.println("分支提交");
     }
 }
