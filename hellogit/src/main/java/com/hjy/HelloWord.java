85a950dbb09a67d44934c9c03c583b0542996659
120117hjy <50613338+120117hjy@users.noreply.github.com>
2020-09-18 10:42:26 +0800

    Update HelloWord.java

--- hellogit/src/main/java/com/hjy/HelloWord.java
+++ hellogit/src/main/java/com/hjy/HelloWord.java
@@ -5,6 +5,6 @@ public class HelloWord {
     public static void main(String[] args) {
         System.out.print("第一个版本");
         System.out.println("第二个版本");
-        System.out.println("master分支共同提交1");
+        System.out.println("master分支共同提交啦啦啦");
     }
 }
